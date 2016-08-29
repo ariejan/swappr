71deb5bcc5ee16d9ad7ca796140dfaab56b9a63a
Ariejan de Vroom <ariejan@devroom.io>
2016-08-29 10:44:43 +0200

    wip

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
+
   factory :user do
-    
+    sequence(:email) { |n| "john-#{n}@example.com" }
+    password              "tester123"
+    password_confirmation "tester123"
   end
+
 end
--- spec/features/sign_ups_spec.rb
+++ spec/features/sign_ups_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.feature "sign up", type: :feature do
 
   context 'devise sign up' do
+    let(:email) { "john.doe@example.com" }
+
     before do
       visit '/users/sign_up'
-      fill_in "user_email", with: "john.doe@example.com"
+      fill_in "user_email", with: email
       fill_in "user_password", with: "tester123"
       fill_in "user_password_confirmation", with: "tester123"
       click_button 'Sign up'
@@ -16,7 +18,29 @@ RSpec.feature "sign up", type: :feature do
     end
 
     it "sends a confirmation email" do
-      expect(unread_emails_for('john.doe@example.com').size).to eq(1)
+      expect(unread_emails_for(email).size).to eq(1)
+      open_email(email)
+      expect(current_email).to have_subject("Confirmation instructions")
+    end
+
+    xit "allows confirmation through email" do
+    end
+  end
+
+  context 'step 1 - fill out profile' do
+    let(:user) { create :user }
+
+    before { sign_in(user) }
+
+    it 'handles valid input' do
+      visit '/after_signup/profile'
+      fill_in 'user_full_name', with: 'John X. Doe'
+      fill_in 'user_mobile_number', with: '06-11223344'
+      check 'user_whatsapp'
+      click_button 'Next'
+
+      expect(current_path).to eq('/after_signup/address')
+
     end
   end
 end
